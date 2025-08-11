fa675ebedde1d295e49448dad741a77578398d00
scintilla <falsegigantoca@gmail.com>
2025-08-11 23:58:27 +0800

    DateTime component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { Time } from "./component/DateTime";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-black">Hello, World!</h1>
+      <div className="bg-[#2D2D2D] min-h-screen">
+        <nav className="text-white">Nav</nav>
+        <Time />
+        <div> </div>
+      </div>
     </>
   );
 }
